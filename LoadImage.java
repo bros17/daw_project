a62727ed484d0628f7050d74182cf1aa0cd8aff1
albert <albert@gmail.com>
2015-10-19 14:40:35 +0200

    v6

--- LoadImage.java
+++ LoadImage.java
@@ -32,5 +32,19 @@ public class LoadImageApp extends Component {
                      }
             }
    
-            
+public static void main(String[] args) {
+   
+                        JFrame f = new JFrame("Load Image Sample");
+                                       
+                        f.addWindowListener(new WindowAdapter(){
+                                                public void windowClosing(WindowEvent e) {
+                                                            System.exit(0);
+                                                }
+                                    });
+   
+                        f.add(new LoadImageApp());
+                        f.pack();
+                        f.setVisible(true);
+} 
+           
 }
